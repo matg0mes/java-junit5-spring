e0a9baced417b5b7de1cc58c8a7dd8b4d46db3c5
matg0mes <hotmus8500@gmail.com>
2022-09-26 19:29:23 -0300

    feat(vet): create tests to controllers

---
+++ src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
@@ -0,0 +1,68 @@
+package org.springframework.samples.petclinic.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.samples.petclinic.model.Vet;
+import org.springframework.samples.petclinic.model.Vets;
+import org.springframework.samples.petclinic.service.ClinicService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+class VetControllerTest {
+
+    @Mock
+    ClinicService clinicService;
+
+    @Mock
+    Map<String, Object> model;
+
+    @InjectMocks
+    VetController vetController;
+
+    List<Vet> vetList = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        vetList.add(new Vet());
+
+        given(clinicService.findVets()).willReturn(vetList);
+    }
+
+    @Test
+    void showVetList() {
+        //when
+        String view = vetController.showVetList(model);
+
+        //then
+        then(clinicService).should().findVets();
+        then(model).should().put(anyString(), any(Vets.class));
+        assertThat(view).isEqualToIgnoringCase("vets/vetList");
+    }
+
+    @Test
+    void showResourcesVetList() {
+        //when
+        Vets result = vetController.showResourcesVetList();
+
+        //then
+        then(clinicService).should().findVets();
+        assertThat(result.getVetList()).isEqualTo(vetList);
+        assertThat(result.getVetList()).hasSize(1);
+    }
+}
